4e10b83036beba6264f23f0b1ec9ae91d384a82a
ViktorHalas <99_vita@mail.ru>
2023-04-17 17:27:12 +0300

    feat(project-4): add BudgetContext

---
+++ src/context/ExpensesContext/BudgetContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext,ReactNode, useContext, useState } from "react";
+
+interface BudgetContextState {
+  budget: number;
+  saveBudget: (budget: string) => void;
+}
+
+interface BudgetContextProviderProps {
+  children: ReactNode;
+}
+
+const BudgetContext = createContext<BudgetContextState>({} as BudgetContextState);
+
+export const useBudgetContextValue = () => {
+  const [budgetValue, setBudgetContextValue] = useState<BudgetContextState>(() => {
+    return {
+      budget: 0,
+      saveBudget: (budget) => {
+        setBudgetContextValue((context) => ({ ...context, budget: Number(budget) }));
+      },
+    };
+  });
+  return budgetValue;
+};
+
+export const useBudgetContext = () => useContext(BudgetContext);
+
+export const BudgetContextProvider = ({ children }: BudgetContextProviderProps) => {
+  return (
+    <BudgetContext.Provider value={useBudgetContextValue()}>{children}</BudgetContext.Provider>
+  );
+};
